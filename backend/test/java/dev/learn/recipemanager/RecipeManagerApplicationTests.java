9359fbe24a456c9676f87cfd12efc4722f78c68f
im24b-antonyd <antonyd@bzz.ch>
2026-06-04 13:15:35 +0200

    Initalising Recipe-Manger project.

---
+++ backend/test/java/dev/learn/recipemanager/RecipeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.learn.recipemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
